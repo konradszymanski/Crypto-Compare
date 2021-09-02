277f49b894a361b08f910dc8808830a8be13eb79
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-09-02 20:03:50 +0100

    refactor getData

--- script.js
+++ script.js
@@ -7,28 +7,48 @@ const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=
 
 let coin;
 
-const getData = () => {
+const getData = async () => {
 
     coin = input.value.toLowerCase();
-
-    fetch(`${url}${coin}`)
-        .then(res => { return res.json() })
-        .then(data => {
-            console.log(data)
-            takeParameters(data)
-        })
-        .catch(err => typo())
+    const json = await fetch(`${url}${coin}`)
+    const res = await json.json()
+    console.log(res)
+    takeParameters(res)
 }
+
 const typo = () => {
     resultsContainer.textContent = `Name of the coin is wrong typed`
 }
 const takeParameters = data => {
-    data.forEach( params => {
-        resultsContainer.innerHTML +=`${params.id} ${params.symbol} ${params.name}  <img src="${params.image}"/>
-    ${params.current_price} ${params.market_cap}
-    ${params.market_cap_rank} ${params.fully_diluted_valuation} ${params.total_volume} ${params.high_24h} ${params.low_24h} ${params.price_change_percentage_24h} ${params.market_cap_change} ${params.market_cap_change_24h}
-    ${params.circulating_supply} ${params.total_supply} ${params.max_supply} ${params.ath} ${params.loath_change_percentagew_24h} ${params.ath_date} ${params.atl} ${params.atl_change_percentage} ${params.atl_date} ${params.last_updated} ${params.price_change_24h}</br>` 
-    }) 
+    data.forEach(params => {
+        resultsContainer.innerHTML += `
+            ${params.id}</br>
+            ${params.symbol}</br>
+            ${params.name}</br>  
+            <img src="${params.image}"/></br>
+            ${params.current_price}</br> 
+            ${params.market_cap}</br>
+            ${params.market_cap_rank}</br> 
+            ${params.fully_diluted_valuation}</br> 
+            ${params.total_volume}</br> 
+            ${params.high_24h}</br> 
+            ${params.low_24h}</br> 
+            ${params.price_change_percentage_24h}</br> 
+            ${params.market_cap_change_24h}</br> 
+            ${params.market_cap_change_percentage_24h}</br>
+            ${params.circulating_supply}</br> 
+            ${params.total_supply}</br> 
+            ${params.max_supply}</br> 
+            ${params.ath}</br> 
+            ${params.ath_change_percentage}</br> 
+            ${params.ath_date}</br> 
+            ${params.atl}</br> 
+            ${params.atl_change_percentage}</br> 
+            ${params.atl_date}</br> 
+            ${params.last_updated}</br> 
+            ${params.price_change_24h}</br>`
+    })
+    
 }
 
 submitButton.addEventListener('click', getData)
@@ -52,7 +72,7 @@ submitButton.addEventListener('click', getData)
 //  total_supply = data[0].total_supply,
 //  max_supply = data[0].max_supply,
 //  ath = data[0].ath,
-//  loath_change_percentagew_24h = data[0].ath_change_percentage,
+//  loath_change_percentage_24h = data[0].ath_change_percentage,
 //  ath_date = data[0].ath_date,.
 //  atl = data[0].atl,
 //  atl_change_percentage = data[0].atl_change_percentage,
