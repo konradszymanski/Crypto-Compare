c2a78db48818c82ff24d443430ba93156d156e94
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-09-09 22:15:00 +0100

    update first column details

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ const takeParameters = data => {
 
     data.forEach(p => {
         coins.push(
+            p.symbol.toUpperCase(),
             p.current_price,
             p.image = `<img src = ${p.image}>`,
             p.market_cap_rank,
@@ -63,6 +64,7 @@ const takeParameters = data => {
             p.total_volume,
             p.high_24h,
             p.low_24h,
+            p.price_change_24h,
             p.price_change_percentage_24h,
             p.market_cap_change_24h,
             p.market_cap_change_percentage_24h,
@@ -75,8 +77,7 @@ const takeParameters = data => {
             p.atl,
             p.atl_change_percentage,
             p.atl_date,
-            p.last_updated,
-            p.price_change_24h,
+            p.last_updated
         )
     })
 }
@@ -104,61 +105,4 @@ const emptyArray = (array) => {
 }
 
 submitButton.addEventListener('click', checkInput)
-//data.forEach(params => {
-    //     resultsContainer.innerHTML += `
-    //         ${params.id}</br>
-    //         ${params.symbol}</br>
-    //         ${params.name}</br>  
-    //         <img src="${params.image}"/></br>
-    //         ${params.current_price}</br> 
-    //         ${params.market_cap}</br>
-    //         ${params.market_cap_rank}</br> 
-    //         ${params.fully_diluted_valuation}</br> 
-    //         ${params.total_volume}</br> 
-    //         ${params.high_24h}</br> 
-    //         ${params.low_24h}</br> 
-    //         ${params.price_change_percentage_24h}</br> 
-    //         ${params.market_cap_change_24h}</br> 
-    //         ${params.market_cap_change_percentage_24h}</br>
-    //         ${params.circulating_supply}</br> 
-    //         ${params.total_supply}</br> 
-    //         ${params.max_supply}</br> 
-    //         ${params.ath}</br> 
-    //         ${params.ath_change_percentage}</br> 
-    //         ${params.ath_date}</br> 
-    //         ${params.atl}</br> 
-    //         ${params.atl_change_percentage}</br> 
-    //         ${params.atl_date}</br> 
-    //         ${params.last_updated}</br> 
-    //         ${params.price_change_24h}</br>`
-    // })
 
-
-
-// {
-//  id = data[0].id,
-//  symbol = data[0].symbol,
-//  name = data[0].name,
-//  image = data[0].image,
-//  current_price = data[0].current_price,
-//  market_cap = data[0].market_cap,
-//  market_cap_rank = data[0].market_cap_rank,
-//  fully_diluted_valuation = data[0].fully_diluted_valuation,
-//  total_volume = data[0].total_volume,
-//  high_24h = data[0].high_24h,
-//  low_24h = data[0].low_24h,
-//  price_change_percentage_24h = data[0].price_change_percentage_24h,
-//  market_cap_change = data[0].market_cap_change_24h,
-//  market_cap_change_24h = data[0].market_cap_change_percentage_24h,
-//  circulating_supply = data[0].circulating_supply,
-//  total_supply = data[0].total_supply,
-//  max_supply = data[0].max_supply,
-//  ath = data[0].ath,
-//  loath_change_percentage_24h = data[0].ath_change_percentage,
-//  ath_date = data[0].ath_date,.
-//  atl = data[0].atl,
-//  atl_change_percentage = data[0].atl_change_percentage,
-//  atl_date = data[0].atl_date,
-//  last_updated = data[0].last_updated,
-//  price_change_24h = data[0].price_change_24h,
-// }
